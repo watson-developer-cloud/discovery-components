22af335c84c8b8df1f07ab1dfa4b17fde9b3bc5a
Andrew Turgeon <apturgeo@us.ibm.com>
2019-09-06 14:03:38 -0400

    move search client out of context

--- packages/discovery-components-react/src/components/DiscoverySearch/DiscoverySearch.tsx
+++ packages/discovery-components-react/src/components/DiscoverySearch/DiscoverySearch.tsx
@@ -1,25 +1,43 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import DiscoveryV1 from 'ibm-watson/discovery/v1';
 
 interface DiscoverySearchProps {
-  searchClient: DiscoveryV1
+  searchClient: DiscoveryV1;
 }
-interface SearchContextProps {
-  searchClient: DiscoveryV1
+interface SearchFunc {
+  (nlq: string): void;
+}
+interface ISearchContext {
+  onSearch: SearchFunc;
+  searchResults: DiscoveryV1.QueryResponse;
+  searchParameters: DiscoveryV1.QueryParams;
 }
 
-export const SearchContext = React.createContext<SearchContextProps>({
-  searchClient: new DiscoveryV1({
-    url: 'foo',
-    username: 'bar',
-    password: 'baz',
-    version: '2019-01-01'
-  })
+export const SearchContext = React.createContext<ISearchContext>({
+  onSearch: () => {},
+  searchResults: {},
+  searchParameters: {
+    environment_id: 'foo',
+    collection_id: 'bar'
+  }
 });
 
-export const DiscoverySearch: React.SFC<DiscoverySearchProps> = (props) => {
+export const DiscoverySearch: React.SFC<DiscoverySearchProps> = props => {
+  const [searchResults, setSearchResults] = useState<DiscoveryV1.QueryResponse>({});
+  const [searchParameters, setSearchParameters] = useState<DiscoveryV1.QueryParams>({
+    environment_id: 'foo',
+    collection_id: 'bar'
+  });
+  const handleSearch = async (nlq: string) => {
+    searchParameters.natural_language_query = nlq;
+    setSearchParameters(searchParameters);
+    const searchResults: DiscoveryV1.QueryResponse = await props.searchClient.query(
+      searchParameters
+    );
+    setSearchResults(searchResults);
+  };
   return (
-    <SearchContext.Provider value={{ searchClient: props.searchClient }}>
+    <SearchContext.Provider value={{ onSearch: handleSearch, searchResults, searchParameters }}>
       {props.children}
     </SearchContext.Provider>
   );
--- packages/discovery-components-react/src/components/SearchInput/SearchInput.tsx
+++ packages/discovery-components-react/src/components/SearchInput/SearchInput.tsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import DiscoveryV1 from 'ibm-watson/discovery/v1';
 import { Search as CarbonSearchInput } from 'carbon-components-react';
 import { SearchContext } from '../DiscoverySearch/DiscoverySearch';
 
@@ -9,14 +8,9 @@ interface SearchInputProps {
 
 export const SearchInput: React.SFC<SearchInputProps> = ({ small }) => {
   const searchContext = useContext(SearchContext);
-  const callback = (response: DiscoveryV1.QueryResponse) => window.alert(response);
   const handleOnChange = (value: string) => {
-    return searchContext.searchClient.query({
-      environment_id: 'default',
-      collection_id: 'foo',
-      natural_language_query: value
-    }, callback);
+    searchContext.onSearch(value);
   };
 
   return <CarbonSearchInput small={small} onChange={handleOnChange} />;
-}
+};
