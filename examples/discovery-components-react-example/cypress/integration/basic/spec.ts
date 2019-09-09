0afdba411533500ac2406c6e0b793b8a2c0d40b8
Andrew Turgeon <apturgeo@us.ibm.com>
2019-09-09 16:04:26 -0400

    cypress changes

---
+++ examples/discovery-components-react-example/cypress/integration/basic/spec.ts
@@ -0,0 +1,17 @@
+describe('basic test', () => {
+  beforeEach(() => {
+    cy.server();
+    cy.fixture('basic/query.json').as('queryJSON');
+    cy.route('POST', '**/query?version=2019-01-01', '@queryJSON').as('postQuery');
+  });
+
+  it('loads page', () => {
+    cy.visit('/');
+    cy.get('input').type('abil');
+    cy.wait('@postQuery')
+      .its('url')
+      .should('include', '/query')
+      .its('response.body.natural_language_query')
+      .should('eq', 'abil');
+  });
+});
