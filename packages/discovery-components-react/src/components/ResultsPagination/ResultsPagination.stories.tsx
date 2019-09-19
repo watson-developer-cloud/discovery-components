2ac72fe8a35bd0406133d602936007ccf9b274f4
Chris Gillin <cgillin@us.ibm.com>
2019-09-19 16:40:31 -0400

    Fixup

--- packages/discovery-components-react/src/components/ResultsPagination/ResultsPagination.stories.tsx
+++ packages/discovery-components-react/src/components/ResultsPagination/ResultsPagination.stories.tsx
@@ -4,8 +4,6 @@ import { withKnobs, array } from '@storybook/addon-knobs/react';
 
 import { ResultsPagination } from './ResultsPagination';
 
-// Workaround until we can use addon-smart-knobs for typescript
-// https://github.com/storybookjs/addon-smart-knobs/pull/61
 export const props = () => ({
   page: 1,
   pageSizes: array('Page size choices', [10, 20, 30, 40, 50])
