342947ff0938e51a94657eacf84e522f6f9bdcdb
Steven-Gassert <Steven.Gassert@ibm.com>
2019-09-20 15:32:50 -0400

    lint App.js

--- examples/discovery-components-react-example/src/App.js
+++ examples/discovery-components-react-example/src/App.js
@@ -3,7 +3,12 @@ import DiscoveryV1 from 'ibm-watson/discovery/v1';
 import './app.scss';
 import refinementsQueryResponse from './fixtures/refinementsQueryResponse';
 
-import { DiscoverySearch, SearchInput, SearchRefinements, ResultsPagination } from '@disco-widgets/react-components';
+import {
+  DiscoverySearch,
+  SearchInput,
+  SearchRefinements,
+  ResultsPagination
+} from '@disco-widgets/react-components';
 
 const App = () => {
   // TODO: this is a dummy client to route requests to the server since CP4D doesn't support CORS
