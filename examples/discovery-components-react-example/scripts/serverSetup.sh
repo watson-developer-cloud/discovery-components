7ba04da102541defa33db549e6891e4753ee1795
Chris Gillin <cgillin@us.ibm.com>
2019-09-25 14:43:35 -0400

    Strip protocol from CLUSTER_HOST

--- examples/discovery-components-react-example/scripts/serverSetup.sh
+++ examples/discovery-components-react-example/scripts/serverSetup.sh
@@ -5,12 +5,12 @@ if [[ -z $CLUSTER_USERNAME || -z $CLUSTER_PASSWORD || -z $CLUSTER_HOST || -z $CL
   exit 1
 fi
 
-baseUrl=https://${CLUSTER_HOST}:${CLUSTER_PORT}
+clusterHost=$(echo ${CLUSTER_HOST#*//})
+baseUrl=https://${clusterHost}:${CLUSTER_PORT}
 accessToken=$(curl -s -k -u ${CLUSTER_USERNAME}:${CLUSTER_PASSWORD} ${baseUrl}/v1/preauth/validateAuth -H "Content-Type: application/json" | jq --raw-output '.accessToken')
-releasePath=$(curl -s -k -X POST ${baseUrl}/zen-data/v1/addOn/query -H "Authorization: Bearer ${accessToken}" -d "{}" -H "Content-Type: application/json" | jq --raw-output '.requestObj[] | select(.Type=="discovery") | .Details.openURL')
-resourceInstance=$(curl -s -k -H "Authorization: Bearer ${accessToken}" -X GET "${baseUrl}/${releasePath}/api/ibmcloud/resource-controller/resource_instances?resource_id=discovery" -H "Content-Type: application/json")
+releasePath=$(curl -s -k -X POST ${baseUrl}/zen-data/v1/addOn/query -H "Authorization: Bearer ${accessToken}" -d "{}" -H "Content-Type: application/json" | jq --raw-output '.requestObj[] | select(.Type=="discovery") | .Details.openURL' | sed 's/\/watson//')
+resourceInstance=$(curl -s -k -H "Authorization: Bearer ${accessToken}" -X GET "${baseUrl}/watson/${releasePath}/api/ibmcloud/resource-controller/resource_instances?resource_id=discovery" -H "Content-Type: application/json")
 instanceId=$(echo ${resourceInstance} | jq --raw-output '.resources[].zen_id')
-releasePath=$(echo ${releasePath} | sed 's/\/watson//')
 
 cat >.server-env <<EOL
 RELEASE_PATH=${releasePath}instances/${instanceId}/api
