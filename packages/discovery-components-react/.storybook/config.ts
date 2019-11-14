a304e0e3262adfbb44dfe693fe35f22b2bebd227
Andrew Turgeon <apturgeo@us.ibm.com>
2019-11-14 15:51:28 -0500

    fix package lock

--- packages/discovery-components-react/.storybook/config.ts
+++ packages/discovery-components-react/.storybook/config.ts
@@ -2,14 +2,17 @@ import { configure, addParameters } from '@storybook/react';
 import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 import '@disco-widgets/styles';
 
+const req =
+  process.env.STORYBOOK_BUILD_MODE == 'production'
+    ? require.context('../src', true, /.*\/__stories__\/.*\.stories.(tsx|mdx)$/)
+    : require.context('../src', true, /\.stories.(tsx|mdx)$/);
+
 addParameters({
   docs: {
     container: DocsContainer,
     page: DocsPage
   }
 });
-
-const req = require.context('../src', true, /\.stories.(tsx|mdx)$/);
 
 function loadStories() {
   req.keys().forEach(req);
