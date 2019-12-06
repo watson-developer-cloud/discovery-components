d38870fd4f11d52e3b09c79d561f9b3effc5dc71
Steven-Gassert <stevengassert94@gmail.com>
2019-12-06 16:57:41 -0500

    chore(eslint): disable onFormatSave and add jsx-a11y

--- packages/discovery-react-components/src/components/CIDocument/components/Section/Section.tsx
+++ packages/discovery-react-components/src/components/CIDocument/components/Section/Section.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
 /**
  * A section of a CI (HTML) document. Rendered as a row within a virtual
  * "infinite" list.
