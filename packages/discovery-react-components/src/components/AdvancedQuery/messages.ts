08eece0ceca2347af4c44ba5cb9960cf28c50690
Kathryn Beaty <kabeaty@gmail.com>
2019-12-09 13:19:07 -0500

    feat: initial adding of AdvancedQuery

---
+++ packages/discovery-react-components/src/components/AdvancedQuery/messages.ts
@@ -0,0 +1,11 @@
+export interface Messages {
+  satisfyRulesDropdownText: string;
+  satisfyRulesDropdownAllOptionText: string;
+  satisfyRulesDropdownAnyOptionText: string;
+}
+
+export const defaultMessages: Messages = {
+  satisfyRulesDropdownText: 'Satisfy %dropdown% of the following rules',
+  satisfyRulesDropdownAllOptionText: 'all',
+  satisfyRulesDropdownAnyOptionText: 'any'
+};
