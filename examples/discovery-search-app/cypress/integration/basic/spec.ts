93c866d5f13c23d6c97ead3c02768e7534179856
Noah Eigenfeld <noah.eigenfeld@ibm.com>
2019-12-10 16:39:17 -0500

    test: restructure test files

--- examples/discovery-search-app/cypress/integration/basic/spec.ts
+++ examples/discovery-search-app/cypress/integration/basic/spec.ts
@@ -1,10 +1,41 @@
 describe('basic test', () => {
   beforeEach(() => {
-    cy.server();
+    cy.server({ force404: true });
     cy.fixture('basic/collections.json').as('collectionsJSON');
     cy.route('GET', '**/collections?version=2019-01-01', '@collectionsJSON').as('getCollections');
+    cy.fixture('basic/query.json').as('queryJSON');
+    cy.route('POST', '**/query?version=2019-01-01', '@queryJSON').as('postQuery');
     cy.visit('/');
   });
 
-  //TODO: 'example app doesn't crash' test
+  // Rendering initial page
+  describe('When the example app loads', () => {
+    it('SearchInput has placeholder text "Search"', () => {
+      cy.get('.bx--search-input').should('have.attr', 'placeholder', 'Search');
+    });
+
+    it('SearchInput has magnifying glass icon', () => {
+      cy.get('.bx--search-magnifier').should('be.visible');
+    });
+
+    // TODO: top entities
+
+    // TODO: collections selector
+  });
+
+  // Basic query tests
+  describe('When entering a query', () => {
+    beforeEach(() => {
+      cy.get('.bx--search-input').type('abil{enter}');
+    });
+
+    it('makes the appropriate query request', () => {
+      cy.wait('@postQuery')
+        .its('requestBody.count')
+        .should('eq', 0);
+      cy.wait('@postQuery')
+        .its('requestBody.natural_language_query')
+        .should('eq', 'abil');
+    });
+  });
 });
