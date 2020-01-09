b9a015b119c9c948e19872eb986af8f469cb58e7
Kathryn Beaty <kabeaty@gmail.com>
2020-01-09 13:25:56 -0500

    feat: structured query add and remove group rules (#42)
    * feat: update to initially add in groups to data structure
    
    * feat: get adding groups initially working
    
    * feat: some refactoring and getting remove group/group rules working
    
    * feat: refactor RemoveRuleRowButton out into its own component
    
    * feat: break RuleGroup out into own component
    
    * feat: pull AddRuleGroupButton out into own component plus refactoring
    
    * test: add initial nested rulegroups tests
    
    * feat: some refactorings
    
    * feat: some cleanup and refactorings
    
    * docs: updates for consistency
    
    * test: add tests for adding/removing from multiple rule groups
    
    * test: add test that only max num rule groups can be added
    
    * feat: refactor tests and other cleanup
    
    * test: improve test organization
    
    * test: add another expect for removing rule test
    
    * test: few more tweaks
    
    * feat: update data structure for groups per PR feedback
    
    * refactor: dont need to pass rows to RuleGroup
    
    * refactor: improve naming and rowId not optional
    
    * refactor: use max instead of last in arr for new rulerowid
    
    * refactor: improve naming
    
    * refactor: update with group_order and to address other PR feedback
    
    Co-authored-by: Andrew Turgeon <andrew.p.turgeon@gmail.com>

---
+++ packages/discovery-react-components/src/components/StructuredQuery/components/RuleGroup/RuleGroup.tsx
@@ -0,0 +1,76 @@
+import React, { FC, Dispatch, SetStateAction } from 'react';
+import { Messages } from 'components/StructuredQuery/messages';
+import { RuleGroupDropdown } from '../RuleGroupDropdown/RuleGroupDropdown';
+import { RuleRow } from '../RuleRow/RuleRow';
+import { AddRuleRowButton } from '../AddRuleRowButton/AddRuleRowButton';
+import { StructuredQuerySelection } from 'components/StructuredQuery/utils/structuredQueryInterfaces';
+import { MAX_NUM_SIBLING_RULE_ROWS } from 'components/StructuredQuery/constants';
+import {
+  structuredQueryRuleGroupClass,
+  structuredQueryNestedRuleGroupClass,
+  structuredQueryRulesButtonsClass
+} from 'components/StructuredQuery/cssClasses';
+
+export interface RuleGroupProps {
+  /**
+   * override default messages for the component by specifying custom and/or internationalized text strings
+   */
+  messages: Messages;
+  /**
+   * id of the group for the rule row to render, or 'top-level' if the top-level rule group
+   */
+  groupId: number;
+  /**
+   * state that represents the current rules and selections for the structured query
+   */
+  structuredQuerySelection: StructuredQuerySelection;
+  /**
+   * used to set the structuredQuerySelection state
+   */
+  setStructuredQuerySelection: Dispatch<SetStateAction<StructuredQuerySelection>>;
+}
+
+export const RuleGroup: FC<RuleGroupProps> = ({
+  messages,
+  groupId,
+  structuredQuerySelection,
+  setStructuredQuerySelection
+}) => {
+  const rows = structuredQuerySelection.groups[groupId].rows;
+  const isTopLevelGroup = groupId === 0;
+  const showAddRuleRowButton = rows.length < MAX_NUM_SIBLING_RULE_ROWS && !isTopLevelGroup;
+  const ruleGroupClassNames = [structuredQueryRuleGroupClass];
+  if (!isTopLevelGroup) {
+    ruleGroupClassNames.push(structuredQueryNestedRuleGroupClass);
+  }
+
+  return (
+    <div className={ruleGroupClassNames.join(' ')} data-testid="rule-group">
+      <RuleGroupDropdown messages={messages} />
+      {rows.map(row => {
+        const uniqueKey = groupId.toString() + row.toString();
+        return (
+          <RuleRow
+            messages={messages}
+            groupId={groupId}
+            rowId={row}
+            key={uniqueKey}
+            setStructuredQuerySelection={setStructuredQuerySelection}
+            structuredQuerySelection={structuredQuerySelection}
+          />
+        );
+      })}
+      <div className={structuredQueryRulesButtonsClass}>
+        {showAddRuleRowButton && (
+          <AddRuleRowButton
+            addRuleRowText={messages.addRuleRowText}
+            key={groupId}
+            groupId={groupId}
+            setStructuredQuerySelection={setStructuredQuerySelection}
+            structuredQuerySelection={structuredQuerySelection}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
