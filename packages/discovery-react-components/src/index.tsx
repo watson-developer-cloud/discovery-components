8ccf0c5ad0687cf3b3913a84ce680fef9d8eb37e
Javier Pedemonte <javier@pedemonte.us>
2020-01-13 10:43:03 -0600

    feat: export canRender func

--- packages/discovery-react-components/src/index.tsx
+++ packages/discovery-react-components/src/index.tsx
@@ -7,6 +7,9 @@ export { SearchInput } from './components/SearchInput/SearchInput';
 export { SearchFacets } from './components/SearchFacets/SearchFacets';
 export { ResultsPagination } from './components/ResultsPagination/ResultsPagination';
 export { DocumentPreview } from './components/DocumentPreview/DocumentPreview';
-export { HtmlView } from './components/DocumentPreview/components/HtmlView/HtmlView';
+export {
+  HtmlView,
+  canRenderHtmlView
+} from './components/DocumentPreview/components/HtmlView/HtmlView';
 export { SearchResults } from './components/SearchResults/SearchResults';
 export { CIDocument, canRenderCIDocument } from './components/CIDocument/CIDocument';
