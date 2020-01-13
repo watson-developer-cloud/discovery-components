a73666093e39b215f6081eeb4b2b8f6c8b241878
Javier Pedemonte <javier@pedemonte.us>
2020-01-12 19:02:04 -0600

    feat: highlight tables in HTML

--- packages/discovery-react-components/src/components/DocumentPreview/components/HtmlView/HtmlView.stories.tsx
+++ packages/discovery-react-components/src/components/DocumentPreview/components/HtmlView/HtmlView.stories.tsx
@@ -4,10 +4,38 @@ import { storiesOf } from '@storybook/react';
 import HtmlView from './HtmlView';
 import docJson from '../../__fixtures__/Art Effects Koya Creative Base TSA 2008.pdf.json';
 
-storiesOf('DocumentPreview/components/HtmlView', module).add('default', () => {
-  return (
-    <div style={{ height: '100%' }}>
-      <HtmlView document={docJson} setLoading={(): void => {}} />
-    </div>
-  );
-});
+storiesOf('DocumentPreview/components/HtmlView', module)
+  .add('default', () => {
+    return (
+      <div style={{ height: '100%' }}>
+        <HtmlView document={docJson} setLoading={(): void => {}} />
+      </div>
+    );
+  })
+  .add('table highlighting', () => {
+    const docWithTable = {
+      ...docJson,
+      table_results: [
+        {
+          table_id: '558ada041262d5b0aa02a05429d798c7',
+          source_document_id: '7e8ada041262d5b0aa02a05429d798c7',
+          collection_id: '8713a92b-28aa-b291-0000-016ddc68aa2a',
+          table_html: '<table><tr><th>Hello</th><tr><td>How are ya?</td></tr></table>',
+          table_html_offset: 42500,
+          table: {
+            location: {
+              begin: 341939,
+              end: 344508
+            }
+          }
+        }
+      ]
+    };
+    const highlight = docWithTable.table_results[0];
+
+    return (
+      <div style={{ height: '100%' }}>
+        <HtmlView document={docWithTable} highlight={highlight} setLoading={(): void => {}} />
+      </div>
+    );
+  });
--- packages/discovery-react-components/src/components/DocumentPreview/components/HtmlView/HtmlView.tsx
+++ packages/discovery-react-components/src/components/DocumentPreview/components/HtmlView/HtmlView.tsx
@@ -1,14 +1,21 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC, useEffect, useState, useRef } from 'react';
 import { settings } from 'carbon-components';
-import { QueryResult } from 'ibm-watson/discovery/v2';
+import { QueryResult, QueryTableResult } from 'ibm-watson/discovery/v2';
 import DOMPurify from 'dompurify';
 import get from 'lodash/get';
+import processDoc from '../../../../utils/document/processDoc';
+import { findOffsetInDOM, createFieldRects } from '../../../../utils/document/documentUtils';
+import { clearNodeChildren } from '../../../../utils/dom';
 
 interface Props {
   /**
    * Document data returned by query
    */
   document: QueryResult;
+  /**
+   * table to highlight in document. Reference to item with `document.table_results`
+   */
+  highlight?: QueryTableResult;
   /**
    * Check to disable toolbar in parent
    */
@@ -19,7 +26,7 @@ interface Props {
   setHideToolbarControls?: (disabled: boolean) => void;
 }
 
-export const canRenderHtmlView = (document?: QueryResult) => get(document, 'html');
+export const canRenderHtmlView = (document?: QueryResult): boolean => !!get(document, 'html');
 
 const SANITIZE_CONFIG = {
   ADD_TAGS: ['bbox'],
@@ -32,25 +39,99 @@ const SANITIZE_CONFIG = {
 
 const base = `${settings.prefix}--html`;
 
-export const HtmlView: FC<Props> = ({ document, setHideToolbarControls, setLoading }) => {
+export const HtmlView: FC<Props> = ({
+  document,
+  highlight,
+  setHideToolbarControls,
+  setLoading
+}) => {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const highlightRef = useRef<HTMLDivElement>(null);
+
   if (setHideToolbarControls) {
     setHideToolbarControls(true);
   }
 
-  const docHtml = get(document, 'html');
-  const html = useMemo(() => (docHtml ? DOMPurify.sanitize(docHtml, SANITIZE_CONFIG) : ''), [
-    docHtml
-  ]);
+  const [html, setHtml] = useState<string | null>(null);
+  useEffect(() => {
+    if (document) {
+      const docHtml = document.html;
+      const html = docHtml ? DOMPurify.sanitize(docHtml, SANITIZE_CONFIG) : '';
 
-  setLoading(false);
+      if (html && highlight) {
+        const process = async (): Promise<void> => {
+          const processedDoc = await processDoc({ ...document, html }, { sections: true });
+          const fullHtml = processedDoc.sections
+            ? processedDoc.sections.map(section => section.html).join('')
+            : '';
+          setHtml(`
+          <div>
+            <style>${processedDoc.styles}</style>
+            ${fullHtml}
+          </div>`);
+        };
+        process();
+      } else {
+        setHtml(html);
+      }
+    }
+    setLoading(false);
+  }, [document, highlight, setLoading]);
+
+  // highlight table and scroll into view
+  useEffect(() => {
+    if (!html) {
+      return;
+    }
+
+    const contentNode = contentRef.current;
+    const highlightNode = highlightRef.current;
+
+    if (highlightNode) {
+      clearNodeChildren(highlightNode);
+    }
+
+    if (!highlight || !contentNode || !highlightNode) {
+      return;
+    }
+
+    const { begin, end } = get(highlight, 'table.location');
+    if (typeof begin === 'undefined' || typeof end === 'undefined') {
+      return;
+    }
+
+    const offsets = findOffsetInDOM(contentNode, begin, end);
+
+    const fragment = window.document.createDocumentFragment();
+    const parentRect = contentNode.getBoundingClientRect() as DOMRect;
+    createFieldRects({
+      fragment,
+      parentRect,
+      fieldType: 'highlight',
+      fieldId: begin.toString(),
+      ...offsets
+    });
+    highlightNode.appendChild(fragment);
+
+    // scroll highlight into view
+    const firstFieldRect = highlightNode.querySelector('.field--rect');
+    if (firstFieldRect) {
+      firstFieldRect.scrollIntoView({ block: 'center' });
+    }
+  }, [html, highlight]);
 
   return (
-    <div
-      className={base}
-      dangerouslySetInnerHTML={{
-        __html: html
-      }}
-    />
+    <div className={base}>
+      <div ref={highlightRef} />
+      {html && (
+        <div
+          dangerouslySetInnerHTML={{
+            __html: html
+          }}
+          ref={contentRef}
+        />
+      )}
+    </div>
   );
 };
 
