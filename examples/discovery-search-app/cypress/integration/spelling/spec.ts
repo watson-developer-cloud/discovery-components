c883a310488d45b6a70a7a925deae21751405f15
Noah Eigenfeld <noaheig@gmail.com>
2020-01-22 15:52:55 -0500

    test: Add Cypress Tests for Facets (#45)
    * test: add collection filter tests
    
    * test: create skeleton for single facet tests
    
    * test: populate tests for single-select facets
    
    * test: fill out tests for multi-select facets
    
    * test: add tests for dynamic facets
    
    * test: initial requested changes
    
    * test: bring cypress-testing-library to our cypress tests
    
    * test: update tests to work with cypress testing library
    
    * test: add check for array length
    
    * chore: try increasing the cypress version
    
    * fix: temp fix for flakey test
    
    * fix: implement requested changes
    
    Co-authored-by: Kathryn Beaty <kabeaty@gmail.com>

--- examples/discovery-search-app/cypress/integration/spelling/spec.ts
+++ examples/discovery-search-app/cypress/integration/spelling/spec.ts
@@ -23,7 +23,7 @@ describe('Spelling', () => {
 
   describe('When entering a misspelled query', () => {
     beforeEach(() => {
-      cy.get('.bx--search-input').type('waston{enter}');
+      cy.findByLabelText('Search').type('waston{enter}');
       cy.wait('@postQueryMisspelled');
     });
 
@@ -38,6 +38,7 @@ describe('Spelling', () => {
         cy.route('POST', '**/query?version=2019-01-01', '@correctedQueryJSON').as(
           'postQueryCorrected'
         );
+        cy.contains('There were no results found').click(); // Makes this test less flakey, but there's a race condition in SearchInput we should solve at some point
         cy.get('button')
           .contains('watson')
           .click();
