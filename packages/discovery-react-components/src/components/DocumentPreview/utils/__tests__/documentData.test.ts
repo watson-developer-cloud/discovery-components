6d1d472021c6350fdd35dbd2c634a4ff7049cce3
Heather Finnegan <Heather.Finnegan@ibm.com@Heathers-MacBook-Pro.local>
2020-01-29 09:49:03 -0600

    chore: add test to check for result values to documentData

--- packages/discovery-react-components/src/components/DocumentPreview/utils/__tests__/documentData.test.ts
+++ packages/discovery-react-components/src/components/DocumentPreview/utils/__tests__/documentData.test.ts
@@ -4,7 +4,7 @@ import jsonDoc from '../../__fixtures__/Art Effects Koya Creative Base TSA 2008.
 const correctTextMapping = {
   extracted_metadata: {
     text_mappings:
-      '{"text_mappings":[{"page":{"page_number":1,"bbox":[54.0,89.32704162597656,558.0890502929688,175.20001363754272]},"field":{"name":"text","index":0,"span":[0,757]}}'
+      '{"text_mappings":[{"page":{"page_number":1,"bbox":[54.0,89.32704162597656,558.0890502929688,175.20001363754272]},"field":{"name":"text","index":0,"span":[0,757]}}],"pages":[{"page_number":0,"height":792.0,"width":612.0,"origin":"TopLeft"}]}'
   }
 };
 
@@ -38,14 +38,18 @@ describe('documentData', () => {
 
   it('succeeds in getting text mapping from json file', () => {
     const mappings = getTextMappings(jsonDoc);
-    console.log(mappings);
     expect(mappings).not.toEqual(null);
     expect(consoleError).not.toHaveBeenCalled();
   });
 
-  it.skip('returns correct text mapping data', () => {
-    const mappings = getTextMappings(correctTextMapping);
-    console.log(mappings);
+  it('returns correct text mapping data', () => {
+    const mappings = getTextMappings(correctTextMapping)!;
+    expect(mappings).not.toEqual(null);
+    expect(mappings.pages[0].page_number).toEqual(0);
+    expect(mappings.pages[0].height).toEqual(792);
+    expect(mappings.pages[0].width).toEqual(612);
+    expect(mappings.pages[0].origin).toEqual('TopLeft');
+    expect(consoleError).not.toHaveBeenCalled();
   });
 
   it('fails gracefully if text mappings does not exist', () => {
