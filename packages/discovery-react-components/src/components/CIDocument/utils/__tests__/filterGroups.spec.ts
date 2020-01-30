be2afaaeb3203d50df1b5e1b3b309729931358e1
Heather Finnegan <Heather.Finnegan@ibm.com@Heathers-MacBook-Pro.local>
2020-01-30 14:40:44 -0600

    fix: typo

--- packages/discovery-react-components/src/components/CIDocument/utils/__tests__/filterGroups.spec.ts
+++ packages/discovery-react-components/src/components/CIDocument/utils/__tests__/filterGroups.spec.ts
@@ -1,7 +1,7 @@
 import { getBaseFilterGroups, FilterGroupWithFns } from '../filterGroups';
 
 describe('filterGroups', () => {
-  it('handles invalid ducment types correctly', () => {
+  it('handles invalid document types correctly', () => {
     const invalidDocType = getBaseFilterGroups('foobar', { FILTER_GROUP_A: 'Filter Group A' });
     expect(invalidDocType).toEqual([]);
   });
