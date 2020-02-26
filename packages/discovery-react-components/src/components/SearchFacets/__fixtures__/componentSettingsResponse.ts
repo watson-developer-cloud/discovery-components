19296c6ac9ebdc631e3c5c390020c3e57775983f
Javier Pedemonte <javier@pedemonte.us>
2020-02-26 11:36:25 -0600

    fix: prettier errors

--- packages/discovery-react-components/src/components/SearchFacets/__fixtures__/componentSettingsResponse.ts
+++ packages/discovery-react-components/src/components/SearchFacets/__fixtures__/componentSettingsResponse.ts
@@ -1,16 +1,16 @@
 import { Response, ComponentSettingsResponse } from 'ibm-watson/discovery/v2';
 import { createDummyResponse } from 'utils/testingUtils';
 
-const aggregationComponentSettingsResponse: Response<
-  ComponentSettingsResponse
-> = createDummyResponse({
-  aggregations: [
-    {
-      name: 'category_id',
-      label: 'Category (label created in component settings)',
-      multiple_selections_allowed: false
-    }
-  ]
-});
+const aggregationComponentSettingsResponse: Response<ComponentSettingsResponse> = createDummyResponse(
+  {
+    aggregations: [
+      {
+        name: 'category_id',
+        label: 'Category (label created in component settings)',
+        multiple_selections_allowed: false
+      }
+    ]
+  }
+);
 
 export default aggregationComponentSettingsResponse;
