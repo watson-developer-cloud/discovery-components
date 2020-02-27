4a98e685f6bd7b4e1bcc6c97f17140ddd3caee77
Broulaye Doumbia <Broulaye.Doumbia@ibm.com@Broulayes-MacBook-Pro-2.local>
2020-02-27 16:18:56 -0600

    fix: make test more readable

--- packages/discovery-react-components/src/components/SearchResults/utils/__tests__/getDocumentTitle.test.tsx
+++ packages/discovery-react-components/src/components/SearchResults/utils/__tests__/getDocumentTitle.test.tsx
@@ -1,46 +1,41 @@
-import React from 'react';
-import { render } from '@testing-library/react';
 import { getDocumentTitle } from '../index';
-
-const mockResultWithTitle = {
-  document_id: 'df4c085040a9049ddf956ca6a213699d',
-  extracted_metadata: {
-    file_type: 'html',
-    title: 'Family Day 2020 (Israel)'
-  }
-};
-
-const mockResultWithoutTitle = {
-  document_id: 'df4c085040a9049ddf956ca6a213699d',
-  extracted_metadata: {
-    file_type: 'html'
-  }
-};
-
-const mockResultWithFilename = {
-  document_id: 'df4c085040a9049ddf956ca6a213699d',
-  extracted_metadata: {
-    file_type: 'html',
-    filename: 'Family Day 2020 (Israel)'
-  }
-};
 
 describe('getDocumentTitle', () => {
   test('returns result title', () => {
+    const mockResultWithTitle = {
+      document_id: 'df4c085040a9049ddf956ca6a213699d',
+      extracted_metadata: {
+        file_type: 'html',
+        title: 'Family Day 2020 (Israel)'
+      }
+    };
     expect(getDocumentTitle('title', mockResultWithTitle)).toEqual(
       mockResultWithTitle.extracted_metadata.title
     );
   });
 
-  test('returns result document id', () => {
-    expect(getDocumentTitle('title', mockResultWithoutTitle)).toEqual(
-      mockResultWithoutTitle.document_id
+  test('returns result file name if title is not available', () => {
+    const mockResultWithFilename = {
+      document_id: 'df4c085040a9049ddf956ca6a213699d',
+      extracted_metadata: {
+        file_type: 'html',
+        filename: 'Family Day 2020 (Israel)'
+      }
+    };
+    expect(getDocumentTitle('title', mockResultWithFilename)).toEqual(
+      mockResultWithFilename.extracted_metadata.filename
     );
   });
 
-  test('returns result file name', () => {
-    expect(getDocumentTitle('title', mockResultWithFilename)).toEqual(
-      mockResultWithFilename.extracted_metadata.filename
+  test('returns result document id if title and filename are not available', () => {
+    const mockResultWithoutTitle = {
+      document_id: 'df4c085040a9049ddf956ca6a213699d',
+      extracted_metadata: {
+        file_type: 'html'
+      }
+    };
+    expect(getDocumentTitle('title', mockResultWithoutTitle)).toEqual(
+      mockResultWithoutTitle.document_id
     );
   });
 });
