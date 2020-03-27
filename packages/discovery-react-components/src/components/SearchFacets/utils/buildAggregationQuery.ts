f649961fa70f0408eeefb14b28577ce821f435e0
Kathryn Beaty <kabeaty@gmail.com>
2020-03-27 14:24:56 -0400

    refactor: some renaming and adding constants

--- packages/discovery-react-components/src/components/SearchFacets/utils/buildAggregationQuery.ts
+++ packages/discovery-react-components/src/components/SearchFacets/utils/buildAggregationQuery.ts
@@ -1,28 +1,23 @@
 import DiscoveryV2 from 'ibm-watson/discovery/v2';
+import { QueryTermAggregationWithName } from './searchFacetInterfaces';
 
-export interface QueryAggregationResults extends DiscoveryV2.QueryAggregation {
-  field?: string;
-  count?: number;
-  name?: string;
-}
+const TOP_ENTITIES_FIELD = 'enriched_text.entities.text';
+const ENTITIES_TYPE_NESTED_TERM = '.term(enriched_text.entities.type,count:1)';
 
 export const buildAggregationQuery = (
-  configuration?: QueryAggregationResults[],
+  configuration?: QueryTermAggregationWithName[],
   searchParamsAgg?: DiscoveryV2.QueryParams['aggregation']
 ): string => {
   if (!searchParamsAgg) {
     const aggregation = configuration!.map(term => {
       const termCount = term.count ? ',count:' + term.count : '';
       const termName = term.name ? ',name:' + term.name : '';
-      const topEntitiesType =
-        term.field === 'enriched_text.entities.text'
-          ? '.term(enriched_text.entities.type,count:1)'
-          : '';
+      const topEntitiesType = term.field === TOP_ENTITIES_FIELD ? ENTITIES_TYPE_NESTED_TERM : '';
       return 'term(' + term.field + termCount + termName + ')' + topEntitiesType;
     });
     return '[' + aggregation!.toString() + ']';
   } else {
-    if (searchParamsAgg.includes('enriched_text.entities.text')) {
+    if (searchParamsAgg.includes(TOP_ENTITIES_FIELD)) {
       const searchParamsAggArr = searchParamsAgg
         .replace('[', '')
         .replace(']', '')
@@ -30,11 +25,11 @@ export const buildAggregationQuery = (
       return (
         '[' +
         searchParamsAggArr
-          .map(agg => {
-            if (agg.includes('enriched_text.entities.text')) {
-              return (agg += '.term(enriched_text.entities.type,count:1)');
+          .map(aggTerm => {
+            if (aggTerm.includes(TOP_ENTITIES_FIELD)) {
+              return (aggTerm += ENTITIES_TYPE_NESTED_TERM);
             }
-            return agg;
+            return aggTerm;
           })
           .join(',term') +
         ']'
