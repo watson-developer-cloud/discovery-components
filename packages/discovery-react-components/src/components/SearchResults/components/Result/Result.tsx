817bedcf73f6f230f0c8dfb85f25613a5d16b1c8
Juan Nicolas Garcia <32341300+JuanNicolasGarcia@users.noreply.github.com>
2020-04-14 14:00:56 -0500

    fix: footer title field length expands parent component too much (#123)
    * fix: footer title length
    
    * fix: update branch
    
    * fix: requested change
    
    * fix: update footer title width
    
    Co-authored-by: Juan Nicolas Garcia <juan.nicolas.garcia@ibm.com>

--- packages/discovery-react-components/src/components/SearchResults/components/Result/Result.tsx
+++ packages/discovery-react-components/src/components/SearchResults/components/Result/Result.tsx
@@ -119,7 +119,11 @@ export const Result: React.FunctionComponent<ResultProps> = ({
   const hasText = displayedText && !showTablesOnlyResults;
   const emptyResultContent = !(hasText || tableHtml);
 
-  const title = getDocumentTitle(result, resultTitleField);
+  let title = getDocumentTitle(result, resultTitleField);
+
+  if (Array.isArray(title)) {
+    title = title[0]; // only first element will be shown if title is array
+  }
 
   const searchResultClasses = [searchResultClass];
   if (isEqual(result, selectedResult.document)) {
