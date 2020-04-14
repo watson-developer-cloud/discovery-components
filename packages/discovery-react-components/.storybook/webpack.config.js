5a20290b52ff0b4d78f879fddb8b9b810829a575
Juan Nicolas Garcia <juan.nicolas.garcia@ibm.com>
2020-04-14 13:03:01 -0500

    fix: stash changes

--- packages/discovery-react-components/.storybook/webpack.config.js
+++ packages/discovery-react-components/.storybook/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const SRC_PATH = path.join(__dirname, '../src');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const styles = require.resolve('@ibm-watson/discovery-styles');
 module.exports = ({ config }) => {
@@ -39,10 +38,7 @@ module.exports = ({ config }) => {
   config.resolve.plugins.push(
     new TsconfigPathsPlugin({
       configFile: path.resolve(__dirname, 'tsconfig.json')
-    }),
-    new CopyWebpackPlugin([
-      { from: 'node_modules/pdfjs-dist/cmaps/', to: 'cmaps/' }
-    ])
+    })
   );
   return config;
 };
