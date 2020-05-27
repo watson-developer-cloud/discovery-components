595f8f9a397b52d6be11be18ac9e2cef1c4c8fd6
H-Fin <heatherkayfinnegan@gmail.com>
2020-05-27 14:15:57 -0500

    fix: search facet entities now include nested

--- packages/discovery-react-components/src/components/SearchFacets/utils/__tests__/buildAggregationQuery.test.ts
+++ packages/discovery-react-components/src/components/SearchFacets/utils/__tests__/buildAggregationQuery.test.ts
@@ -27,21 +27,21 @@ describe('BuildAggregationQuery', () => {
   test('it converts configuration with multiple terms, count, and name, including Top Entities, to expected aggregation parameter', () => {
     const aggParam = buildAggregationQuery(configurationWithTopEntities);
     expect(aggParam).toEqual(
-      '[term(enriched_text.entities.text,count:12,name:entities).term(enriched_text.entities.type,count:1),term(author)]'
+      '[nested(enriched_text.entities).term(enriched_text.entities.text,count:12,name:entities).term(enriched_text.entities.type,count:1),term(author)]'
     );
   });
 
   test('it converts configuration with nested query aggregation to expected aggregation parameter', () => {
     const aggParam = buildAggregationQuery(configurationWithNestedQueryAggregation);
     expect(aggParam).toEqual(
-      '[term(enriched_text.entities.text,count:12,name:entities).term(enriched_text.entities.type,count:1),term(author),nested(enriched_text.entities).filter(enriched_text.entities.model_name:"Dictionary:.products").term(enriched_text.entities.text,count:4,name:dict_ypKBKYnM8LOq)]'
+      '[nested(enriched_text.entities).term(enriched_text.entities.text,count:12,name:entities).term(enriched_text.entities.type,count:1),term(author),nested(enriched_text.entities).filter(enriched_text.entities.model_name:"Dictionary:.products").term(enriched_text.entities.text,count:4,name:dict_ypKBKYnM8LOq)]'
     );
   });
 
   test('it converts configuration with filter query aggregation to expected aggregation parameter', () => {
     const aggParam = buildAggregationQuery(configurationWithFilterQueryAggregation);
     expect(aggParam).toEqual(
-      '[term(enriched_text.entities.text,count:12,name:entities).term(enriched_text.entities.type,count:1),term(author),nested(enriched_text.entities.enriched_text.entities.text).filter(enriched_text.entities.enriched_text.entities.model_name:"Dictionary:.test").term(enriched_text.entities.enriched_text.entities.text,count:4,name:dict_yqYQPpM8OljE)]'
+      '[nested(enriched_text.entities).term(enriched_text.entities.text,count:12,name:entities).term(enriched_text.entities.type,count:1),term(author),nested(enriched_text.entities.enriched_text.entities.text).filter(enriched_text.entities.enriched_text.entities.model_name:"Dictionary:.test").term(enriched_text.entities.enriched_text.entities.text,count:4,name:dict_yqYQPpM8OljE)]'
     );
   });
 });
