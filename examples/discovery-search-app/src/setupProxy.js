894e27b9de9bd610e5755ef8a21ac0e9dd880b4e
noah-eigenfeld <noaheig@gmail.com>
2020-06-04 15:13:24 -0400

    feat: throw error if env files not found

--- examples/discovery-search-app/src/setupProxy.js
+++ examples/discovery-search-app/src/setupProxy.js
@@ -8,18 +8,18 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 if (serverEnv.error) {
-  console.warn(serverEnv.error);
+  throw new Error(serverEnv.error);
 }
 
 if (clusterEnv.error) {
-  console.warn(clusterEnv.error);
+  throw new Error(clusterEnv.error);
 }
 
-const RELEASE_PATH = process.env.RELEASE_PATH;
-const BASE_URL = process.env.BASE_URL;
+const RELEASE_PATH = process.env.RELEASE_PATH || '/example/release/path';
+const BASE_URL = process.env.BASE_URL || 'http://example.com';
 const addAuthorization = async (req, _res, next) => {
   const authenticator = new CloudPakForDataAuthenticator({
-    url: BASE_URL || 'http://example.com',
+    url: BASE_URL,
     username: process.env.CLUSTER_USERNAME || 'username',
     password: process.env.CLUSTER_PASSWORD || 'password',
     disableSslVerification: true
