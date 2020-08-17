664c1e4450c724fa56245b7a60d1a5c88dfc1e3d
Javier Pedemonte <javier@pedemonte.us>
2020-08-17 17:34:25 -0500

    test: consolidate to improve speed and flakiness

--- examples/discovery-search-app/cypress/integration/dynamic_facets/spec.ts
+++ examples/discovery-search-app/cypress/integration/dynamic_facets/spec.ts
@@ -1,7 +1,7 @@
 import { mockHomePage } from '../../support/utils';
 
 describe('Dynamic Facets', () => {
-  beforeEach(() => {
+  it('When a query is made, and dynamic facets are returned', () => {
     mockHomePage({
       component_settings: 'component_settings/multiSelectFacetsComponentSettings.json',
       query: 'query/facetsQuery.json'
@@ -13,144 +13,108 @@ describe('Dynamic Facets', () => {
     cy.fixture('query/facetsQueryDynamicAndNonDynamic.json').as(
       'facetsQueryDynamicAndNonDynamicJSON'
     );
-  });
 
-  describe('When a query is made, and dynamic facets are returned', () => {
-    beforeEach(() => {
-      cy.findByLabelText('Search').type('restaurants{enter}');
-      cy.wait('@postQuery');
-      cy.get('.bx--search-facet')
-        .filter(':contains("Dynamic Facets")')
-        .as('dynamicFacets');
-      // set aliases for the dynamic facet filters
-      cy.get('@dynamicFacets')
-        .contains('regression')
-        .as('regressionFilter');
-      cy.get('@dynamicFacets')
-        .contains('classification')
-        .as('classificationFilter');
-      cy.get('@dynamicFacets')
-        .contains('naive bayes')
-        .as('naiveBayesFilter');
-    });
+    cy.findByLabelText('Search').type('restaurants{enter}');
+    cy.wait('@postQuery');
+    cy.get('.bx--search-facet')
+      .filter(':contains("Dynamic Facets")')
+      .as('dynamicFacets');
+    // set aliases for the dynamic facet filters
+    cy.get('@dynamicFacets')
+      .contains('regression')
+      .as('regressionFilter');
+    cy.get('@dynamicFacets')
+      .contains('classification')
+      .as('classificationFilter');
+    cy.get('@dynamicFacets')
+      .contains('naive bayes')
+      .as('naiveBayesFilter');
 
-    it('shows available dynamic facets listed on the side', () => {
-      cy.get('@regressionFilter').should('exist');
-      cy.get('@classificationFilter').should('exist');
-      cy.get('@naiveBayesFilter').should('exist');
-    });
+    // shows available dynamic facets listed on the side
+    cy.get('@regressionFilter').should('exist');
+    cy.get('@classificationFilter').should('exist');
+    cy.get('@naiveBayesFilter').should('exist');
 
-    it('has dynamic facets displayed as checkboxes', () => {
-      cy.get('@dynamicFacets')
-        .find('.bx--checkbox')
-        .should('have.length', 3);
-    });
+    // has dynamic facets displayed as checkboxes
+    cy.get('@dynamicFacets')
+      .find('.bx--checkbox')
+      .should('have.length', 3);
 
-    describe('and a dynamic facet filter is selected', () => {
-      beforeEach(() => {
-        cy.route('POST', '**/query?version=2019-01-01', '@facetsQuerySingleRefinementJSON').as(
-          'postQueryFacetsSingle'
-        );
-        cy.get('label')
-          .contains('regression')
-          .click();
-        cy.wait('@postQueryFacetsSingle').as('singleRefinementQueryObject');
-      });
+    // and a dynamic facet filter is selected
+    cy.route('POST', '**/query?version=2019-01-01', '@facetsQuerySingleRefinementJSON').as(
+      'postQueryFacetsSingle'
+    );
+    cy.get('label')
+      .contains('regression')
+      .click();
 
-      it('makes a query for the right facets', () => {
-        cy.get('@singleRefinementQueryObject')
-          .its('requestBody.filter')
-          .should('eq', '"regression"');
-      });
+    // makes a query for the right facets
+    cy.wait('@postQueryFacetsSingle')
+      .its('requestBody.filter')
+      .should('eq', '"regression"');
 
-      it('shows the bubble next to dynamic facets saying 1', () => {
-        cy.get('.bx--list-box__selection')
-          .contains('1')
-          .should('exist');
-      });
+    // shows the bubble next to dynamic facets saying 1
+    cy.get('.bx--list-box__selection')
+      .contains('1')
+      .should('exist');
 
-      describe('and a different filter is selected from dynamic facets', () => {
-        beforeEach(() => {
-          cy.route('POST', '**/query?version=2019-01-01', '@facetsQueryMultiRefinementJSON').as(
-            'postQueryMultiRefinement'
-          );
-          cy.get('label')
-            .contains('classification')
-            .click();
-          cy.wait('@postQueryMultiRefinement').as('multiRefinementQueryObject');
-        });
+    // and a different filter is selected from dynamic facets
+    cy.route('POST', '**/query?version=2019-01-01', '@facetsQueryMultiRefinementJSON').as(
+      'postQueryMultiRefinement'
+    );
+    cy.get('label')
+      .contains('classification')
+      .click();
 
-        it('makes a query for both filters', () => {
-          cy.get('@multiRefinementQueryObject')
-            .its('requestBody.filter')
-            .should('eq', '"regression","classification"');
-        });
+    // makes a query for both filters
+    cy.wait('@postQueryMultiRefinement')
+      .its('requestBody.filter')
+      .should('eq', '"regression","classification"');
 
-        it('shows the bubble next to dynamic facets saying 2', () => {
-          cy.get('.bx--list-box__selection')
-            .contains('2')
-            .should('exist');
-        });
-      });
+    // shows the bubble next to dynamic facets saying 2
+    cy.get('.bx--list-box__selection')
+      .contains('2')
+      .should('exist');
 
-      describe('and the "Clear all" button is clicked', () => {
-        beforeEach(() => {
-          cy.route('POST', '**/query?version=2019-01-01', '@queryJSON').as('postQueryFacets');
-          cy.get('button')
-            .contains('Clear all')
-            .click();
-          cy.wait('@postQueryFacets').as('clearedFacetsQueryObject');
-        });
+    // and the "Clear all" button is clicked
+    cy.route('POST', '**/query?version=2019-01-01', '@queryJSON').as('postQueryFacets');
+    cy.get('button')
+      .contains('Clear all')
+      .click();
 
-        it('makes a query without any selected facets', () => {
-          cy.get('@clearedFacetsQueryObject')
-            .its('requestBody.filter')
-            .should('eq', '');
-        });
+    // makes a query without any selected facets
+    cy.wait('@postQueryFacets')
+      .its('requestBody.filter')
+      .should('eq', '');
 
-        it('has the "Clear all" button disappear', () => {
-          cy.findByText('Dynamic Facets').should('exist'); // make sure this test doesn't pass when the page is blank
-          cy.get('button')
-            .contains('Clear all')
-            .should('not.exist');
-        });
-      });
+    // has the "Clear all" button disappear
+    cy.findByText('Dynamic Facets').should('exist'); // make sure this test doesn't pass when the page is blank
+    cy.get('button')
+      .contains('Clear all')
+      .should('not.exist');
 
-      describe('and the same facet checkbox is clicked', () => {
-        beforeEach(() => {
-          cy.route('POST', '**/query?version=2019-01-01', '@queryJSON').as('postQueryFacets');
-          cy.get('label')
-            .contains('regression')
-            .click();
-          cy.wait('@postQueryFacets').as('clearedFacetsQueryObject');
-        });
+    // and the same facet checkbox is clicked
+    cy.route('POST', '**/query?version=2019-01-01', '@queryJSON').as('postQueryFacets');
+    cy.get('label')
+      .contains('regression')
+      .click();
 
-        it('makes a query without any selected facets', () => {
-          cy.get('@clearedFacetsQueryObject')
-            .its('requestBody.filter')
-            .should('eq', '');
-        });
-      });
+    // makes a query without any selected facets
+    cy.wait('@postQueryFacets')
+      .its('requestBody.filter')
+      .should('eq', '"regression"');
 
-      describe('and a different filter is clicked from a different facet', () => {
-        beforeEach(() => {
-          cy.route(
-            'POST',
-            '**/query?version=2019-01-01',
-            '@facetsQueryDynamicAndNonDynamicJSON'
-          ).as('postQueryDynamicAndNonDynamic');
-          cy.get('label')
-            .contains('Hancock, MN')
-            .click();
-          cy.wait('@postQueryDynamicAndNonDynamic').as('dynamicAndNonDynamicQueryObject');
-        });
+    // and a different filter is clicked from a different facet
+    cy.route('POST', '**/query?version=2019-01-01', '@facetsQueryDynamicAndNonDynamicJSON').as(
+      'postQueryDynamicAndNonDynamic'
+    );
+    cy.get('label')
+      .contains('Hancock, MN')
+      .click();
 
-        it('makes a query with both filters', () => {
-          cy.get('@dynamicAndNonDynamicQueryObject')
-            .its('requestBody.filter')
-            .should('eq', 'location:"Hancock, MN","regression"');
-        });
-      });
-    });
+    // makes a query with both filters', () => {
+    cy.wait('@postQueryDynamicAndNonDynamic')
+      .its('requestBody.filter')
+      .should('eq', 'location:"Hancock, MN","regression"');
   });
 });
