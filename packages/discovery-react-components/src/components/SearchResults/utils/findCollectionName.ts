559d97901303de7e2108ca2eb3a19eac8fee4483
Andrew Turgeon <apturgeo@us.ibm.com>
2021-01-15 16:59:09 -0500

    refactor: null to undefined

--- packages/discovery-react-components/src/components/SearchResults/utils/findCollectionName.ts
+++ packages/discovery-react-components/src/components/SearchResults/utils/findCollectionName.ts
@@ -2,7 +2,7 @@ import DiscoveryV2 from 'ibm-watson/discovery/v2';
 import get from 'lodash/get';
 
 export const findCollectionName = (
-  collectionResponse: DiscoveryV2.ListCollectionsResponse | null,
+  collectionResponse: DiscoveryV2.ListCollectionsResponse | undefined,
   queryResult: DiscoveryV2.QueryResult | DiscoveryV2.QueryTableResult
 ): string => {
   const collectionId =
