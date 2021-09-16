d5365900d0b35e815c3d8521d41d796662bed0a6
Javier Pedemonte <javier@pedemonte.us>
2021-09-16 18:05:42 -0500

    fix: escape/unescape field name in search filters

--- packages/discovery-react-components/src/components/SearchFacets/utils/escapeFieldName.ts
+++ packages/discovery-react-components/src/components/SearchFacets/utils/escapeFieldName.ts
@@ -1,3 +1,7 @@
+const SPECIAL_CHARS = '[\\^~><:!,|()[\\]* ]';
+const RE_SPECIAL_CHARS_LOOKBEHIND = new RegExp(`(\\\\)?(${SPECIAL_CHARS})`, 'g'); // /(\\)?([\^~><:!,|()[\]* ])/g
+const RE_SPECIAL_CHARS_ESCAPED = new RegExp(`(\\\\${SPECIAL_CHARS})`, 'g');
+
 /**
  * Before field names can be used as part of an aggregation query, the following
  * characters within the name must be escaped: ^ ~ > < : ! , | ( ) [ ] *
@@ -12,8 +16,17 @@ export function escapeFieldName(fieldName: string): string {
   // @see https://caniuse.com/js-regexp-lookbehind
   // return (fieldName || '').replace(/(?<!\\)[\^~><:!,|()[\]* ]/g, (char: string) => `\\${char}`);
   return (fieldName || '').replace(
-    /(\\)?([\^~><:!,|()[\]* ])/g,
+    RE_SPECIAL_CHARS_LOOKBEHIND,
     // don't escape if character (`p2`) was already escaped (`p1`)
     (str: string, p1: string, p2: string) => (p1 ? str : `\\${p2}`)
   );
+}
+
+/**
+ * Unescaped any string returned from `escapeFieldName`
+ * @param fieldName
+ * @returns unescaped field name string
+ */
+export function unescapeFieldName(fieldName: string): string {
+  return (fieldName || '').replace(RE_SPECIAL_CHARS_ESCAPED, (str: string) => str.substr(1, 1));
 }
