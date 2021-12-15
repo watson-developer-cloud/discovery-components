13e91a6eb5aa1858082324baa4bc63e94cc3bbff
Susumu Fukuda <FUKUDAS@jp.ibm.com>
2021-12-15 20:52:22 +0900

    fix: canter align pdf page

--- packages/discovery-react-components/src/components/DocumentPreview/components/PdfViewer/PdfViewer.tsx
+++ packages/discovery-react-components/src/components/DocumentPreview/components/PdfViewer/PdfViewer.tsx
@@ -107,20 +107,22 @@ const PdfViewer: FC<Props> = ({
   const base = `${settings.prefix}--document-preview-pdf-viewer`;
   return (
     <div className={cx(base, className)}>
-      <canvas
-        ref={canvasRef}
-        className={`${base}__canvas`}
-        style={{ width: `${canvasInfo?.width ?? 0}px`, height: `${canvasInfo?.height ?? 0}px` }}
-        width={canvasInfo?.canvasWidth}
-        height={canvasInfo?.canvasHeight}
-      />
-      <PdfViewerTextLayer
-        className={cx(`${base}__text`, textLayerClassName)}
-        loadedPage={loadedPage}
-        scale={scale}
-        setRenderedText={setRenderedText}
-      />
-      {children}
+      <div className={`${base}__wrapper`}>
+        <canvas
+          ref={canvasRef}
+          className={`${base}__canvas`}
+          style={{ width: `${canvasInfo?.width ?? 0}px`, height: `${canvasInfo?.height ?? 0}px` }}
+          width={canvasInfo?.canvasWidth}
+          height={canvasInfo?.canvasHeight}
+        />
+        <PdfViewerTextLayer
+          className={cx(`${base}__text`, textLayerClassName)}
+          loadedPage={loadedPage}
+          scale={scale}
+          setRenderedText={setRenderedText}
+        />
+        {children}
+      </div>
     </div>
   );
 };
