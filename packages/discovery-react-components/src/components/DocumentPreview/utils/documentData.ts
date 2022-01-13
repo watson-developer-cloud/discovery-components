48eebdbe3ae95f2f81919fb0386327ebdd681115
Kenta <wataken@jp.ibm.com>
2022-01-13 18:30:00 +0900

    refactor: make util function to detect preview type

--- packages/discovery-react-components/src/components/DocumentPreview/utils/documentData.ts
+++ packages/discovery-react-components/src/components/DocumentPreview/utils/documentData.ts
@@ -1,6 +1,6 @@
 import get from 'lodash/get';
 import { QueryResult } from 'ibm-watson/discovery/v2';
-import { TextMappings } from '../types';
+import { TextMappings, PreviewType, DiscoveryDocument } from '../types';
 
 /**
  * Get `text_mappings` document property as an object. Usually, this
@@ -37,4 +37,35 @@ export function isCsvFile(doc: QueryResult | null | undefined): boolean {
  */
 export function isJsonFile(doc: QueryResult | null | undefined): boolean {
   return get(doc, 'extracted_metadata.file_type') === 'json';
+}
+
+export function detectPreviewType(document: DiscoveryDocument, file?: string): PreviewType {
+  const fileType = document.extracted_metadata?.file_type;
+  const hasPassage = !!document.document_passages?.[0]?.passage_text;
+
+  // if we have PDF data, render that
+  // otherwise, render fallback document view
+  if (fileType === 'pdf') {
+    if (document.extracted_metadata.text_mappings) {
+      // when using custom SDU model or OOB (CI) model
+      if (file) {
+        return 'PDF';
+      }
+    } else {
+      // when using fast path
+      if (!hasPassage && file) {
+        return 'PDF';
+      } else {
+        return 'SIMPLE';
+      }
+    }
+  }
+
+  const isJsonType = isJsonFile(document);
+  const isCsvType = isCsvFile(document);
+  if (document.html && !isJsonType && !isCsvType) {
+    return 'HTML';
+  }
+
+  return 'SIMPLE';
 }
