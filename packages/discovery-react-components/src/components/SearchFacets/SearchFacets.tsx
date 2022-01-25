81f2ae742fa34b633bd5fafde7840b929d1307ac
Javier Pedemonte <javier@pedemonte.us>
2022-01-25 15:20:31 -0600

    fix: update query for "Clear all" handler

--- packages/discovery-react-components/src/components/SearchFacets/SearchFacets.tsx
+++ packages/discovery-react-components/src/components/SearchFacets/SearchFacets.tsx
@@ -209,15 +209,18 @@ const SearchFacets: FC<SearchFacetsProps> = ({
     if (onChange) {
       onChange(event);
     }
+
     setFacetSelectionState({ filterFields: [], filterDynamic: [] });
     setCollectionSelectionState([]);
-    // We should update to not select with a click
+
+    // TODO We should update to not select with a click
     // when Carbon MultiSelect selection can be controlled and Downshift's action props are exposed
     (
       document.querySelectorAll(
-        `.${settings.prefix}--list-box__selection--multi`
+        `#${facetsId} .${settings.prefix}--tag--filter [role="button"]`
       ) as NodeListOf<HTMLElement>
     ).forEach(element => element.click());
+
     performSearch({ ...searchParameters, collectionIds: [], offset: 0, filter: '' }, false);
   };
 
