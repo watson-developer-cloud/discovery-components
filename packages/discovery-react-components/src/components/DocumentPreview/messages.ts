16f00fed125aaf48ef9692c3679dc74bd9f53daf
Susumu Fukuda <FUKUDAS@jp.ibm.com>
2022-02-16 14:34:56 +0900

    feat: update toolbar style

--- packages/discovery-react-components/src/components/DocumentPreview/messages.ts
+++ packages/discovery-react-components/src/components/DocumentPreview/messages.ts
@@ -2,6 +2,7 @@ export interface Messages {
   noDataMessage?: string;
   errorMessage?: string;
   // toolbar labels
+  formatTotalPages?: (total: number) => string;
   previousPageLabel?: string;
   nextPageLabel?: string;
   zoomInLabel?: string;
@@ -9,10 +10,11 @@ export interface Messages {
   resetZoomLabel?: string;
 }
 
-export const defaultMessages = {
+export const defaultMessages: Required<Messages> = {
   noDataMessage: 'No document data',
   errorMessage: 'Error previewing document',
   // toolbar labels
+  formatTotalPages: total => (total === 1 ? `${total} page` : `${total} pages`),
   previousPageLabel: 'Previous page',
   nextPageLabel: 'Next page',
   zoomInLabel: 'Zoom in',
