c62b057d7ef0df5df05c5d69aab8937741acd44f
John Colby <colbyj@us.ibm.com>
2022-07-21 15:10:52 -0400

    fix: update tests from exact match changes
    - Review fixes
    - Add comments

--- packages/discovery-react-components/src/components/SearchFacets/utils/searchFilterTransform.tsx
+++ packages/discovery-react-components/src/components/SearchFacets/utils/searchFilterTransform.tsx
@@ -8,14 +8,23 @@ import {
   SelectableDynamicFacets
 } from './searchFacetInterfaces';
 
+/*
+ * Utility class for creating and parsing query strings for search filters
+ */
+
 export class SearchFilterTransform {
   // Match any unescaped commas, colons, pipes
   static SPLIT_UNESCAPED_COMMAS = /(?<!\\),/;
   static SPLIT_UNESCAPED_COLONS = /(?<!\\):/;
   static SPLIT_UNESCAPED_PIPES = /(?<!\\)\|/;
+  // Match if the string is surrounded by parens
+  // The substring inside of the parens will be in $1
+  static STRING_IN_PARENS = /^\((.+)\)$/;
   // Match any special characters
+  // @see https://cloud.ibm.com/docs/discovery-data?topic=discovery-data-query-operators
   static CHARS_TO_ESCAPE = /[|",:()[\]<>^*~.!]/g;
   // Match any escaped special characters
+  // The unescaped version of the character will be in $1
   static ESCAPED_CHAR = /\\([|",:()[\]<>^*~.!])/g;
 
   static fromString(filterString: string): SearchFilterFacets {
@@ -70,6 +79,7 @@ export class SearchFilterTransform {
     };
   }
 
+  // Returns a query string by combining field and dynamic query strings
   static toString(facets: SearchFilterFacets): string {
     const fieldFilters = this.fieldsToString(facets.filterFields);
     const dynamicFilters = this.escapeSelectedFacets(facets.filterDynamic, 'text').join(',');
@@ -80,11 +90,14 @@ export class SearchFilterTransform {
     // Remove the parentheses added by escapeSelectedFacets
     // Unescape the special characters within the string
     const unescapedString = escapedString
-      .replace(/^\((.+)\)$/, '$1')
+      .replace(RegExp(SearchFilterTransform.STRING_IN_PARENS), '$1')
       .replace(RegExp(SearchFilterTransform.ESCAPED_CHAR), '$1');
     return unescapedString;
   }
 
+  // Returns a full filter query string from the set of filterFields
+  // Use the includes query operator (:) for all fields
+  // @see https://cloud.ibm.com/docs/discovery-data?topic=discovery-data-query-operators#includes
   static fieldsToString(facets: InternalQueryTermAggregation[]): string {
     const filterStrings: string[] = [];
     facets.forEach(facet => {
