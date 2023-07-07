6ddd76c9e82e0b39fc21b6cd2dc54c90f2c824ed
Noah Eigenfeld <noaheig@gmail.com>
2023-07-07 15:07:06 -0400

    feat: add styling for overlapping highlights in text view (#521)
    * feat: replace prop to style overlapping highlights
    
    * feat: fix any[] typing
    
    * fix: resolve spread operator problem
    
    * fix: updates from pr review

--- packages/discovery-react-components/src/components/CIDocument/components/CIDocumentContent/CIDocumentContent.tsx
+++ packages/discovery-react-components/src/components/CIDocument/components/CIDocumentContent/CIDocumentContent.tsx
@@ -10,7 +10,8 @@ import { SkeletonText } from 'carbon-components-react';
 import Section, { OnFieldClickFn } from '../Section/Section';
 import VirtualScroll from '../VirtualScroll/VirtualScroll';
 import { defaultTheme, Theme } from 'utils/theme';
-import { SectionType, ItemMap, HighlightIdsByColor } from 'components/CIDocument/types';
+import { SectionType, ItemMap, HighlightWithMeta } from 'components/CIDocument/types';
+import { getId as getLocationId } from 'utils/document/idUtils';
 
 const baseClassName = `${settings.prefix}--ci-doc-content`;
 
@@ -29,7 +30,7 @@ export interface CIDocumentContentProps {
   theme?: Theme;
   documentId?: string;
   onItemClick?: OnFieldClickFn;
-  highlightedIdsByColor?: HighlightIdsByColor;
+  combinedHighlights?: HighlightWithMeta[];
 }
 
 const CIDocumentContent: FC<CIDocumentContentProps> = ({
@@ -47,7 +48,7 @@ const CIDocumentContent: FC<CIDocumentContentProps> = ({
   theme = defaultTheme,
   documentId = '',
   onItemClick = (): void => {},
-  highlightedIdsByColor
+  combinedHighlights
 }) => {
   const virtualScrollRef = useRef<any>();
 
@@ -69,12 +70,10 @@ const CIDocumentContent: FC<CIDocumentContentProps> = ({
       ) : (
         <>
           <style data-testid="style">{docStyles}</style>
-          {!!highlightedIdsByColor &&
-            highlightedIdsByColor.length > 0 &&
-            highlightStyling(highlightedIdsByColor).map(highlightStyleRules => {
-              return <style>{highlightStyleRules}</style>;
-            })}
-          {(!highlightedIdsByColor || highlightedIds.length <= 0) && (
+          {!!combinedHighlights && combinedHighlights.length > 0 && (
+            <style>{highlightColoringFullArray(combinedHighlights)}</style>
+          )}
+          {(!combinedHighlights || combinedHighlights.length <= 0) && (
             <style>
               {createStyleRules(highlightedIds, [
                 backgroundColorRule(theme.highlightBackground),
@@ -134,25 +133,20 @@ const CIDocumentContent: FC<CIDocumentContentProps> = ({
   );
 };
 
-function highlightStyling(
-  highlightedLocationIdsByColor: { color: string; highlightLocationIds: string[] }[]
-): string[] {
-  return highlightedLocationIdsByColor.map(highlightData => {
-    const { color, highlightLocationIds } = highlightData;
-
-    return createStyleRules(highlightLocationIds, [
-      backgroundColorRule(color),
-      // Set z-index to -1 in order to push non-active fields back
-      zIndexRule(-1)
-    ]);
-  });
-}
-
 function createStyleRules(idList: string[], rules: string[]): string {
   return idList
     .map(id => `.${baseClassName} .field[data-field-id="${id}"] > *`)
     .join(',')
     .concat(`{${rules.join(';')}}`);
+}
+
+function highlightColoringFullArray(combinedHighlightsWithMeta: HighlightWithMeta[]) {
+  return combinedHighlightsWithMeta.map(highlightWithMeta => {
+    const locationId = getHighlightLocationId(highlightWithMeta);
+    // Set z-index to -1 in order to push non-active fields back
+    const rules = `.${baseClassName} .field[data-field-id="${locationId}"] > * {background-color: ${highlightWithMeta.color}; z-index: -1;}`;
+    return <style>{rules}</style>;
+  });
 }
 
 function backgroundColorRule(color: string): string {
@@ -180,6 +174,15 @@ function scrollToActiveItem(
     itemMap.byItem[activeId],
     `.field[data-field-id="${activeId}"]`
   );
+}
+
+function getHighlightLocationId(highlightWithMeta: HighlightWithMeta): string {
+  return getLocationId({
+    location: {
+      begin: highlightWithMeta.begin,
+      end: highlightWithMeta.end
+    }
+  });
 }
 
 export default CIDocumentContent;
