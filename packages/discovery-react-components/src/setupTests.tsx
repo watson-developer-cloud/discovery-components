c0f655807f077ab0cdc85ff8e0033f1e87af57ea
DORIAN MILLER <millerbd@us.ibm.com>
2023-09-11 16:46:18 -0700

    fix: satisfy git build

--- packages/discovery-react-components/src/setupTests.tsx
+++ packages/discovery-react-components/src/setupTests.tsx
@@ -8,13 +8,20 @@ Element.prototype.scrollIntoView = jest.fn();
 // guidance https://stackoverflow.com/questions/71521976/referenceerror-domrect-is-not-defined
 // globalThis https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis
 globalThis.DOMRect = class DOMRect {
-  bottom = 0;
-  left = 0;
-  right = 0;
-  top = 0;
-  constructor(x = 0, y = 0, width = 0, height = 0) {}
-  static fromRect(other) {
-    return new DOMRect(other.x, other.y, other.width, other.height);
+  bottom: number = 0;
+  left: number = 0;
+  right: number = 0;
+  top: number = 0;
+  width: number = 0;
+  height: number = 0;
+  constructor(x: number = 0, y: number = 0, width: number = 0, height: number = 0) {
+    this.top = x;
+    this.bottom = y + height;
+    this.right = x + width;
+    this.left = x;
+  }
+  static fromRect(other: DOMRect) {
+    return new DOMRect(other.top, other.left, other.width, other.height);
   }
   toJSON() {
     return JSON.stringify(this);
